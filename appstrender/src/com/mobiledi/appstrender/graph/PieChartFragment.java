259263fe96040059131badd36bdea655862da9b4
praks-mobiledi <prakash@mobiledi.com>
2014-08-06 00:31:45 +0530

    Change: Pie Chart Screen

--- appstrender/src/com/mobiledi/appstrender/graph/PieChartFragment.java
+++ appstrender/src/com/mobiledi/appstrender/graph/PieChartFragment.java
@@ -2,6 +2,7 @@ package com.mobiledi.appstrender.graph;
 
 import org.achartengine.GraphicalView;
 
+import com.mobiledi.appstrender.AppObject;
 import com.mobiledi.appstrender.R;
 
 import android.content.pm.PackageStats;
@@ -17,7 +18,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 public class PieChartFragment extends Fragment{
-	
+	long totalUsage;
 	int mCurrentPage;
 	
 	@Override
@@ -29,21 +30,49 @@ public class PieChartFragment extends Fragment{
 		
 		/** Getting integer data of the key current_page from the bundle */
 		mCurrentPage = data.getInt("current_page", 0);
+		totalUsage=getTotalUsage();
 	}
 	
+	private long getTotalUsage() {
+		long usage=0;
+		for(AppObject obj: PieChartActivity.listofApps){
+			
+			usage=usage+obj.getSent()+obj.getRecieved();	
+		}
+		return usage;
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.piechart_layout, container,false);				
 		TextView tv = (TextView) v.findViewById(R.id.tv1);
+		TextView tvS = (TextView) v.findViewById(R.id.tvsent);
+		TextView tvR = (TextView) v.findViewById(R.id.tvrecieved);
 		ImageView iv= (ImageView) v.findViewById(R.id.icons);
 		ShowPieChart sp = new ShowPieChart(getActivity(),
-				PieChartActivity.listofApps.get(mCurrentPage));
+				PieChartActivity.listofApps.get(mCurrentPage),this.totalUsage);
 		GraphicalView Gv = sp.openChart();
 		LinearLayout layout = (LinearLayout) v.findViewById(
 				R.id.showgraph);
 		layout.removeAllViews();
 		//1st parameter was 960
 		layout.addView(Gv, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		long sent=PieChartActivity.listofApps.get(mCurrentPage).getSent()/1048576;
+		long recieved=PieChartActivity.listofApps.get(mCurrentPage).getRecieved()/1048576;
+		if(recieved > 0){
+			tvR.setText("Recieved: " +String.valueOf(recieved)+ " MB");
+			if(sent>0){
+			tvS.setText("Sent: " + String.valueOf(sent) + " MB");
+			}
+			else{
+				tvS.setText("Sent: " + String.valueOf(PieChartActivity.listofApps.get(mCurrentPage).getSent()/1024)+ " KB");
+			}
+		}
+		else{
+			tvS.setText("Sent: " + String.valueOf(PieChartActivity.listofApps.get(mCurrentPage).getSent()/1024)+ " KB");
+			tvR.setText("Received: " +String.valueOf(PieChartActivity.listofApps.get(mCurrentPage).getRecieved()/1024)+ " KB");		
+		}
+		
 		tv.setText("# " + (mCurrentPage+1) + "/"+PieChartActivity.listofApps.size()+ " Swipe Left/Right");	
 		iv.setImageDrawable(PieChartActivity.listofApps.get(mCurrentPage).getIcon());
 		return v;		
--- appstrender/src/com/mobiledi/appstrender/graph/ShowPieChart.java
+++ appstrender/src/com/mobiledi/appstrender/graph/ShowPieChart.java
@@ -15,38 +15,66 @@ import android.util.DisplayMetrics;
 import android.util.TypedValue;
 ///returns a pie chart
 public class ShowPieChart {
-	int dataUsage[] = { 0, 0 };
+	long dataUsage[] = { 0, 0 };
+	long totalUsage=0;
 	String AppName;
 	Context context;
 	
-	public ShowPieChart(Context context, AppObject s) {
+	public ShowPieChart(Context context, AppObject s, long usage) {
 		this.context = context;
-		this.dataUsage[0] = (int) s.getSent();
-		this.dataUsage[1] = (int) s.getRecieved();
+		this.dataUsage[0] =  s.getSent();
+		this.dataUsage[1] =  s.getRecieved();
 		this.AppName = s.getAppName();
-
+		this.totalUsage=usage;
 	}
 
 	//public void openChart() {
 	public GraphicalView openChart() {
 		DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-		float val = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, metrics);
+		float val = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 14, metrics);
 		float val2 = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 18, metrics);
 		//float val3 = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 25, metrics);
 		CategorySeries series = new CategorySeries("pie"); 
-		series.add("Sent", dataUsage[0]);
-		series.add("Recieved", dataUsage[1]);
+		/////////
+		long sent=dataUsage[0]/1048576;
+		long recieved=dataUsage[1]/1048576;
+		if(recieved > 0){
+			if(sent>0){
+				series.add("Sent " +  sent + "MB", dataUsage[0]);
+			}
+			else{
+				series.add("Sent " +  dataUsage[0]/1024 + "KB", dataUsage[0]);
+			}
+			series.add("Rec. "  + recieved + "MB", dataUsage[1]);
+		
+		}
+		else{
+			series.add("Sent " +  dataUsage[0]/1024 + "KB", dataUsage[0]);
+			series.add("Rec. "  + dataUsage[1]/1024 + "KB", dataUsage[1]);
+		}
+	
+		///////////
+		
+		
+		
+		/*series.add("Sent " +  (dataUsage[0]/1024) + "KB", dataUsage[0]);
+		series.add("Rec. "  + (dataUsage[1]/1024) + "KB", dataUsage[1]);*/
 		//int[] colors = new int[] { Color.parseColor("#E6E6DC"), Color.parseColor("#00628B") };
 		int[] colors = new int[] { Color.parseColor("#ee5f5b"), Color.parseColor("#29a1cb")};
 		// set style for series
 		DefaultRenderer renderer = new DefaultRenderer();
 		for (int color : colors) {
 			SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+			//r.setChartValuesSpacing(5);
 			r.setColor(color);
-			r.setDisplayBoundingPoints(true);
-			r.setDisplayChartValuesDistance(5);
+			//r.setDisplayBoundingPoints(true);
+			//
+			r.setDisplayChartValuesDistance(3);
+			r.setDisplayBoundingPoints(false);
+		//
 			r.setDisplayChartValues(true);
 			r.setChartValuesTextSize(val);
+			r.setDisplayChartValuesDistance(4);
 			renderer.addSeriesRenderer(r);
 		}
 		
@@ -56,23 +84,26 @@ public class ShowPieChart {
 		//renderer.setZoomButtonsVisible(true); // set zoom button in Graph
 		renderer.setApplyBackgroundColor(true);
 		//renderer.setBackgroundColor(Color.BLACK); // set background color
-		int x=dataUsage[0] + dataUsage[1];
-		int check=x/1048576;
-		if(check>0)
-		renderer.setChartTitle("Total Data Usage by " + AppName + ": "
-				+ (check + "MB"));
+		long x= (dataUsage[0] + dataUsage[1]);
+		double check=x/1048576.00;
+		if(check>1)
+		renderer.setChartTitle(this.AppName+" data usage : "
+				+ ( Math.round(check * 100.0) / 100.0) + "MB"+"\n Total usage since last reboot: " + (totalUsage/1048576) + "MB");
 		else {
-			renderer.setChartTitle("Total Data Usage by " + AppName + ": "
-					+ (x/1024 + "KB"));
+			renderer.setChartTitle(this.AppName+" data usage : "
+					+ ((Math.round((x/1024.00) * 100.0) / 100.0) + "KB"+"\n Total data usage since reboot: " + (totalUsage/1048576) + "MB"));
 		}	
 		renderer.setLabelsColor(Color.BLACK);
 		renderer.setChartTitleTextSize(val2);
 		renderer.setShowLabels(true);
+		renderer.setStartAngle(47);
 		renderer.setLabelsTextSize(val);
-		renderer.setLegendTextSize(val);
-		renderer.setDisplayValues(true);
+		//renderer.setLegendTextSize(val);
+		//renderer.setDisplayValues(true);
+		renderer.setShowCustomTextGrid(true);
 		renderer.setPanEnabled(false);
-
+		renderer.setShowLegend(false);
+		//renderer.
 	/*	Intent intent = ChartFactory.getPieChartIntent(context, series,
 				renderer, AppName);
 		context.startActivity(intent);*/
